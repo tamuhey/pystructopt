1ae4e9bf36d14377c00bdd060bfb015051c6c644
Yohei Tamura <tamuhey@gmail.com>
2021-07-16 18:52:46 +0900

    init

---
+++ tests/test_main.py
@@ -0,0 +1,19 @@
+from structopt_py import parse, _parse
+from dataclasses import dataclass
+import dataclasses
+from dataclass_utils import check_type
+
+
+@dataclass
+class Opt:
+    a: int
+    b: str
+    c: str = "foo"
+    d: bool = False
+
+
+def test_parse():
+    assert dataclasses.is_dataclass(Opt)
+    args = ["--a", "100", "--b", "2", "--d", "True"]
+    opt = _parse(Opt, args)
+    assert opt == Opt(100, "2", "foo", True)
