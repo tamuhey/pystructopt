c582184a0f4b769f863f8534c56ca1b10fe8672c
Yohei Tamura <tamuhey@gmail.com>
2021-07-17 20:14:29 +0900

    add is_same_class

--- pystructopt/utils.py
+++ pystructopt/utils.py
@@ -1,5 +1,5 @@
 import inspect
-from typing import Any, List, Type, TypeVar
+from typing import Any, List, Type, TypeVar, get_args
 from typing_extensions import TypeGuard, get_origin
 
 T = TypeVar("T")
@@ -8,6 +8,13 @@ T = TypeVar("T")
 def is_same_type(a: Any, b: Type[T]) -> TypeGuard[Type[T]]:
     if a == b:
         return True
-    if not inspect.isclass(a) and get_origin(a) is None:
+    aorigin = get_origin(a) or a
+    aargs = get_args(a)
+    borigin = get_origin(b) or b
+    bargs = get_args(b)
+    if not inspect.isclass(a) and aorigin is None:
+        # not a class and generics
         return False
-    return True
+    c1 = aorigin is borigin
+    c2 = not aargs or not bargs or aargs == bargs
+    return c1 and c2
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,32 @@
+import sys
 from typing import List
 from pystructopt.utils import is_same_type
 import pytest
 
 
-@pytest.mark.parametrize("a,b,expected", [(List[int], list, True)])
+@pytest.mark.parametrize(
+    "a,b,expected",
+    [
+        (List[int], list, True),
+        (set, list, False),
+        (int, str, False),
+        (List[str], List[int], False),
+    ],
+)
 def test_is_same_class(a, b, expected):
     assert is_same_type(a, b) == expected
-    ...
+
+
+if sys.version_info >= (3, 9, 0):
+
+    @pytest.mark.parametrize(
+        "a,b,expected",
+        [
+            (List[int], list[int], True),
+            (List, list, True),
+            (List, list[int], True),
+            (set, list[int], False),
+        ],
+    )
+    def test_is_same_class39(a, b, expected):
+        assert is_same_type(a, b) == expected
