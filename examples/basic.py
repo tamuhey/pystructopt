7e8731de37c08fe291ea05b61d866e53462a8645
Yohei Tamura <tamuhey@gmail.com>
2021-07-19 01:31:43 +0900

    add examples

---
+++ examples/basic.py
@@ -0,0 +1,17 @@
+import pystructopt
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Opts:
+    position1: str
+
+    # optional argument
+    count: int = field(metadata={"short": True, "long": True})
+
+    # -vvv -> 3
+    verbose: int = field(metadata={"short": True, "from_occurrences": True})
+
+
+opts = pystructopt.parse(Opts)
+print(opts)
