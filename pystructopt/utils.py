3faf6b5517e8f846095438a65f20461dc1d152d8
Yohei Tamura <tamuhey@gmail.com>
2021-07-19 01:23:01 +0900

    changed parse API: not return None, but raises PraseError becuase sometimes the result means Ok(None)

--- pystructopt/utils.py
+++ pystructopt/utils.py
@@ -1,40 +1,53 @@
 import inspect
-from typing import Optional, Type, TypeVar, Union, cast
+from typing import Any, Type, TypeVar, Union, cast
 
 from typing_extensions import Literal, get_args, get_origin
 
 T = TypeVar("T")
 
 
-def from_str(kls: Type[T], value: str) -> Optional[T]:
+class ParseError(ValueError):
+    def __init__(self, kls: Type[Any], value: str):
+        self.kls = kls
+        self.value = value
+
+    def __str__(self) -> str:
+        return f"Cannot convert `{self.value}` to {self.kls}"
+
+
+def from_str(kls: Type[T], value: str) -> T:
     if kls is str:
         return cast(T, value)
     if inspect.isclass(kls):
         try:
             return kls(value)  # type: ignore
         except ValueError:
-            return None
+            raise ParseError(kls, value)
 
     origin = get_origin(kls)
     if origin is Literal:
         return _from_str_lit(kls, value)
     if origin is Union:
         return _from_str_union(kls, value)
-    return None
+    raise ParseError(kls, value)
 
 
-def _from_str_union(kls: Type[T], value: str) -> Optional[T]:
+def _from_str_union(kls: Type[T], value: str) -> T:
     for arg in get_args(kls):
-        ret = from_str(arg, value)
-        if ret is not None:
-            return ret
-    return None
+        try:
+            return from_str(arg, value)
+        except ParseError:
+            continue
+    raise ParseError(kls, value)
 
 
-def _from_str_lit(kls: Type[T], value: str) -> Optional[T]:
+def _from_str_lit(kls: Type[T], value: str) -> T:
     for arg in get_args(kls):
         ty = type(arg)
-        ret = from_str(ty, value)
-        if ret == arg:
-            return ret
-    return None
+        try:
+            ret = from_str(ty, value)
+            if ret == arg:
+                return ret
+        except ParseError:
+            continue
+    raise ParseError(kls, value)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 from typing import Type, Union, Any
 from pathlib import Path
 from typing_extensions import Literal
-from pystructopt.utils import from_str
+from pystructopt.utils import ParseError, from_str
 import pytest
 
 
@@ -13,13 +13,26 @@ import pytest
         (Literal[1], "1", 1),
         (Literal[1, 2], "1", 1),
         (Union[Literal[1], Literal["a"]], "1", 1),
-        (int, "a", None),
-        (Literal[1], "2", None),
         (Union[Literal[1], Literal["1"]], "1", 1),
         (Union[Literal[3], Literal["1", 2]], "2", 2),
         (Path, "a", Path("a")),
         (float, "1", 1.0),
+        (float, "1.1", 1.1),
     ],
 )
 def test_from_str(kls: Type[Any], value: Any, expected: Any):
     assert from_str(kls, value) == expected
+
+
+@pytest.mark.parametrize(
+    "kls,value",
+    [
+        (int, "a"),
+        (int, "1.1"),
+        (Literal[1], "2"),
+    ],
+)
+def test_from_str_error(kls: Type[Any], value: Any):
+    with pytest.raises(ParseError):
+        ret = from_str(kls, value)
+        print(ret)
